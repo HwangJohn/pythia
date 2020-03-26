9d5dfa65b3c4bc31b0e9676484799e0558ca1762
HwangJohn <angelic805@gmail.com>
2020-03-26 06:27:14 +0000

    DEBUG test

---
+++ run_vizwiz_train.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=2,3
+export PYTHONPATH=$PYTHONPATH:/tf/notebooks/jwhwang/vqa/pythia
+
+#python tools/run.py --tasks captioning --datasets coco --model butd  --config configs/captioning/coco/butd_nucleus_sampling.yml
+python tools/run.py --tasks captioning --datasets vizwiz --model butd  --config configs/captioning/vizwiz/butd_vizwiz.yml
