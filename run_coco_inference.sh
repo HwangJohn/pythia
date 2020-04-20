c1c19dc7cb7eca414ecd91adfcc40368831a6bee
HwangJohn <angelic805@gmail.com>
2020-04-20 13:27:42 +0000

    FEATURE vizwiz학습 후

--- run_coco_inference.sh
+++ run_coco_inference.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=5,6,7
 export PYTHONPATH=$PYTHONPATH:/tf/notebooks/jwhwang/vqa/pythia
 
 #python tools/run.py --tasks captioning --datasets coco --model butd  --config configs/captioning/coco/butd_nucleus_sampling.yml
-python tools/run.py --tasks captioning --datasets coco --model butd  --config configs/captioning/coco/my_butd.yml
+python tools/run.py --tasks captioning --datasets coco --model butd  --config configs/captioning/coco/my_butd.yml --run_type inference  --resume_file save/captioning_coco_butd/butd_final.pth
--- run_coco_train.sh
+++ run_coco_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=1,2
+export CUDA_VISIBLE_DEVICES=1,2,3,4
 export PYTHONPATH=$PYTHONPATH:/tf/notebooks/jwhwang/vqa/pythia
 
 #python tools/run.py --tasks captioning --datasets coco --model butd  --config configs/captioning/coco/butd_nucleus_sampling.yml
